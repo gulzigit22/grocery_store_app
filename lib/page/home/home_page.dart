6cdbdf3a62ee5ad2ad4ec9adc76c1fed761ef665
gulzigit22 <talantbekuulugulzigit51@gmail.com>
2024-05-23 14:08:59 +0600

    ass

--- lib/page/home/home_page.dart
+++ lib/page/home/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_store_app/page/home/home_page2.dart';
 import 'package:grocery_store_app/page/profil/profil.dart';
-import 'package:grocery_store_app/screen/items_diplay.dart';
 import 'package:grocery_store_app/utils/colors.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,7 +14,7 @@ class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
 
   static final List<Widget> _widgetOptions = <Widget>[
-    const ItemsDisplay(),
+    const HomePage2(),
     ProfilePage()
   ];
 
@@ -28,9 +28,6 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Grocery Store App'),
-        ),
         body: _widgetOptions.elementAt(_selectedIndex),
         resizeToAvoidBottomInset: false,
         backgroundColor: Colors.white,
---
+++ lib/page/home/home_page2.dart
@@ -0,0 +1,411 @@
+import 'package:flutter/material.dart';
+import 'package:grocery_store_app/model/items_model.dart';
+import 'package:grocery_store_app/screen/items_details.dart';
+import 'package:grocery_store_app/utils/colors.dart';
+
+class HomePage2 extends StatefulWidget {
+  const HomePage2({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage2> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage2> {
+  int indexCategory = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: ListView(
+        children: [
+          const SizedBox(height: 16),
+          header(),
+          const SizedBox(height: 30),
+          title(),
+          const SizedBox(height: 20),
+          search(),
+          const SizedBox(height: 30),
+          foodMethod(),
+          //   gridFood(),
+        ],
+      ),
+    );
+  }
+
+  GridView foodMethod() {
+    return GridView.builder(
+      shrinkWrap: true,
+      itemCount: foodsItems.length,
+      physics: const NeverScrollableScrollPhysics(),
+      padding: const EdgeInsets.all(16),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        mainAxisSpacing: 8,
+        crossAxisSpacing: 8,
+        mainAxisExtent: 265,
+      ),
+      itemBuilder: (context, index) {
+        FoodDetail food = foodsItems[index];
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => DetailPage(food: food),
+              ),
+            );
+          },
+          child: Container(
+            height: 265,
+            decoration: BoxDecoration(
+              color: Colors.grey[100],
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Stack(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Center(
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(125),
+                        child: Image.asset(
+                          food.image,
+                          height: 120,
+                          width: 120,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      child: Text(
+                        food.name,
+                        maxLines: 1,
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 19),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      child: Row(
+                        children: [
+                          Text(
+                            food.cookingTime,
+                            style: const TextStyle(color: Colors.black38),
+                          ),
+                          const Spacer(),
+                          const Icon(
+                            Icons.star,
+                            color: Colors.amber,
+                            size: 18,
+                          ),
+                          const SizedBox(
+                            width: 4,
+                          ),
+                          Text(
+                            food.rate.toString(),
+                            style: const TextStyle(color: Colors.black38),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        '\$${food.price}',
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 19,
+                            color: Colors.black),
+                      ),
+                    )
+                  ],
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 150, top: 10),
+                  child: Icon(
+                    Icons.favorite_border,
+                    color: Colors.black45,
+                  ),
+                ),
+                const Align(
+                  alignment: Alignment.bottomRight,
+                  child: Material(
+                    color: primaryColors,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      bottomRight: Radius.circular(20),
+                    ),
+                    child: InkWell(
+                      child: Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Icon(
+                          Icons.add,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget header() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Row(
+        children: [
+          Material(
+            color: Colors.grey[300],
+            borderRadius: BorderRadius.circular(8),
+            child: InkWell(
+              onTap: () {},
+              borderRadius: BorderRadius.circular(8),
+              child: Container(
+                height: 40,
+                width: 40,
+                alignment: Alignment.center,
+                child: const Icon(Icons.menu, color: Colors.black),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget title() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          Text(
+            'Hi Indra',
+            style: TextStyle(
+              color: Colors.green,
+              fontWeight: FontWeight.w500,
+              fontSize: 18,
+            ),
+          ),
+          Text(
+            'Find your food',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 34,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget search() {
+    return Container(
+      height: 60,
+      margin: const EdgeInsets.symmetric(horizontal: 16),
+      padding: const EdgeInsets.fromLTRB(8, 2, 6, 2),
+      decoration: BoxDecoration(
+        color: Colors.green[50],
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                prefixIcon: const Icon(Icons.search, color: Colors.green),
+                hintText: 'Search food',
+                hintStyle: TextStyle(color: Colors.grey[600]),
+              ),
+            ),
+          ),
+          Material(
+            color: Colors.green,
+            borderRadius: BorderRadius.circular(10),
+            child: InkWell(
+              onTap: () {},
+              borderRadius: BorderRadius.circular(10),
+              child: Container(
+                width: 50,
+                height: 50,
+                alignment: Alignment.center,
+                child: const Icon(Icons.bar_chart, color: Colors.white),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget categories() {
+    List list = ['Food', 'Fruits', 'Vegetables', 'Grocery', 'Drink'];
+    return SizedBox(
+      height: 40,
+      child: ListView.builder(
+        itemCount: list.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              indexCategory = index;
+              setState(() {});
+            },
+            child: Container(
+              padding: EdgeInsets.fromLTRB(
+                index == 0 ? 16 : 16,
+                0,
+                index == list.length - 1 ? 16 : 16,
+                0,
+              ),
+              alignment: Alignment.center,
+              child: Text(
+                list[index],
+                style: TextStyle(
+                  fontSize: 22,
+                  color: indexCategory == index ? Colors.green : Colors.grey,
+                  fontWeight: indexCategory == index ? FontWeight.bold : null,
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  // Widget gridFood() {
+  //   return GridView.builder(
+  //     itemCount: dummyFoods.length,
+  //     shrinkWrap: true,
+  //     physics: const NeverScrollableScrollPhysics(),
+  //     padding: const EdgeInsets.all(16),
+  //     gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+  //       crossAxisCount: 2,
+  //       mainAxisSpacing: 8,
+  //       crossAxisSpacing: 8,
+  //       mainAxisExtent: 261,
+  //     ),
+  //     itemBuilder: (context, index) {
+  //       Food food = dummyFoods[index];
+  //       return GestureDetector(
+  //         onTap: () {
+  //           Navigator.push(context, MaterialPageRoute(builder: (context) {
+  //             return DetailPage(food: food);
+  //           }));
+  //         },
+  //         child: Container(
+  //           height: 261,
+  //           decoration: BoxDecoration(
+  //             color: Colors.grey[200],
+  //             borderRadius: BorderRadius.circular(16),
+  //           ),
+  //           child: Stack(
+  //             children: [
+  //               Column(
+  //                 crossAxisAlignment: CrossAxisAlignment.start,
+  //                 children: [
+  //                   const SizedBox(height: 16),
+  //                   Center(
+  //                     child: ClipRRect(
+  //                       borderRadius: BorderRadius.circular(120),
+  //                       child: Image.asset(
+  //                         food.image,
+  //                         width: 120,
+  //                         height: 120,
+  //                         fit: BoxFit.cover,
+  //                       ),
+  //                     ),
+  //                   ),
+  //                   const SizedBox(height: 16),
+  //                   Padding(
+  //                     padding: const EdgeInsets.symmetric(horizontal: 16),
+  //                     child: Text(
+  //                       food.name,
+  //                       style: Theme.of(context).textTheme.headline6,
+  //                       textAlign: TextAlign.center,
+  //                       maxLines: 1,
+  //                       overflow: TextOverflow.ellipsis,
+  //                     ),
+  //                   ),
+  //                   const SizedBox(height: 4),
+  //                   Padding(
+  //                     padding: const EdgeInsets.symmetric(horizontal: 16),
+  //                     child: Row(
+  //                       children: [
+  //                         Text(
+  //                           food.cookingTime,
+  //                           style: TextStyle(color: Colors.grey[600]),
+  //                         ),
+  //                         const Spacer(),
+  //                         const Icon(Icons.star, color: Colors.amber, size: 18),
+  //                         const SizedBox(width: 4),
+  //                         Text(
+  //                           food.rate.toString(),
+  //                           style: TextStyle(color: Colors.grey[600]),
+  //                         ),
+  //                       ],
+  //                     ),
+  //                   ),
+  //                   Padding(
+  //                     padding: const EdgeInsets.all(16),
+  //                     child: Text(
+  //                       '\$${food.price}',
+  //                       style: const TextStyle(
+  //                         color: Colors.black,
+  //                         fontWeight: FontWeight.bold,
+  //                         fontSize: 20,
+  //                       ),
+  //                     ),
+  //                   ),
+  //                 ],
+  //               ),
+  //               const Positioned(
+  //                 top: 12,
+  //                 right: 12,
+  //                 child: Icon(Icons.favorite_border, color: Colors.grey),
+  //               ),
+  //               const Align(
+  //                 alignment: Alignment.bottomRight,
+  //                 child: Material(
+  //                   color: Colors.green,
+  //                   borderRadius: BorderRadius.only(
+  //                     topLeft: Radius.circular(16),
+  //                     bottomRight: Radius.circular(16),
+  //                   ),
+  //                   child: InkWell(
+  //                     child: Padding(
+  //                       padding: EdgeInsets.all(8),
+  //                       child: Icon(Icons.add, color: Colors.white),
+  //                     ),
+  //                   ),
+  //                 ),
+  //               ),
+  //             ],
+  //           ),
+  //         ),
+  //       );
+  //     },
+  //   );
+}
+// }
